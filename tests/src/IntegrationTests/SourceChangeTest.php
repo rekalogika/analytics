9a2793caf54e45a13d09060d5964f13d93366ed7
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-02-04 13:17:37 +0700

    test: entity modification

--- tests/src/IntegrationTests/SourceChangeTest.php
+++ tests/src/IntegrationTests/SourceChangeTest.php
@@ -17,12 +17,14 @@ use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
 use Rekalogika\Analytics\Model\Entity\DirtyFlag;
 use Rekalogika\Analytics\PivotTable\LeafNode;
+use Rekalogika\Analytics\Query\SummaryItem;
 use Rekalogika\Analytics\SummaryManagerRegistry;
 use Rekalogika\Analytics\Tests\App\Entity\Customer;
 use Rekalogika\Analytics\Tests\App\Entity\Item;
 use Rekalogika\Analytics\Tests\App\Entity\Order;
 use Rekalogika\Analytics\Tests\App\Entity\OrderSummary;
 use Rekalogika\Analytics\Tests\App\EventListener\TestNewDirtyFlagListener;
+use Rekalogika\Analytics\TimeDimensionHierarchy\Year;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Clock\Test\ClockSensitiveTrait;
 use Zenstruck\Messenger\Test\InteractsWithMessenger;
@@ -59,6 +61,44 @@ class SourceChangeTest extends KernelTestCase
             ->getResult();
 
         $child = $result->getChildren()[0]
+            ?? throw new \RuntimeException('No children found');
+        $this->assertInstanceOf(LeafNode::class, $child);
+        $count = $child->getValue();
+        $this->assertIsInt($count);
+
+        return $count;
+    }
+
+    private function getOrderCountIn2030(): int
+    {
+        $summaryManager = static::getContainer()
+            ->get(SummaryManagerRegistry::class)
+            ->getManager(OrderSummary::class);
+
+        $result = $summaryManager->createQuery()
+            ->groupBy('time.year')
+            ->select('count')
+            ->getResult();
+
+        $children = $result->getChildren();
+
+        $subItem = null;
+
+        foreach ($children as $c) {
+            $item = $c->getItem();
+            $this->assertInstanceOf(Year::class, $item);
+
+            if ((string) $item === '2030') {
+                $subItem = $c;
+                break;
+            }
+        }
+
+        if (!$subItem instanceof SummaryItem) {
+            return 0;
+        }
+
+        $child = $subItem->getChildren()[0]
             ?? throw new \RuntimeException('No children found');
         $this->assertInstanceOf(LeafNode::class, $child);
         $count = $child->getValue();
@@ -177,4 +217,75 @@ class SourceChangeTest extends KernelTestCase
         // dump($transport->queue()->messages());
     }
 
+    public function testSourceModification(): void
+    {
+        $clock = self::mockTime();
+
+        $entityManager = static::getContainer()
+            ->get(EntityManagerInterface::class);
+
+        // get the current result
+
+        $this->assertEquals(0, $this->getOrderCountIn2030());
+
+        // get one order and modify the time to 2030
+
+        $order = $entityManager
+            ->getRepository(Order::class)
+            ->findOneBy([])
+            ?? throw new \RuntimeException('Order not found');
+
+        $order->setTime(new \DateTimeImmutable('2030-02-01 00:00:00'));
+
+        // flush
+
+        $entityManager->flush();
+        $entityManager->clear();
+
+        // check dirty flag
+
+        $dirtyFlags = $entityManager
+            ->getRepository(DirtyFlag::class)
+            ->findAll();
+
+        $this->assertCount(1, $dirtyFlags);
+        $dirtyFlag = $dirtyFlags[0];
+        $this->assertEquals(OrderSummary::class, $dirtyFlag->getClass());
+        $this->assertNotNull($dirtyFlag->getKey());
+        $this->assertNotNull($dirtyFlag->getLevel());
+
+        // check if NewDirtyFlagEvent is emitted
+
+        $listener = static::getContainer()->get(TestNewDirtyFlagListener::class);
+        $this->assertCount(1, $listener->getEvents());
+
+        // check messenger now, it should have one pending message
+
+        $transport = $this->transport('async');
+        $transport->process();
+        $transport->queue()->assertCount(1);
+
+        // check messenger in 59 seconds, should not consume the message
+
+        $clock->sleep(59);
+        $transport->process();
+        $transport->queue()->assertCount(1);
+
+        // in the exact 60th second it should process the message, and generates
+        // the corresponding secondary refresh command
+
+        $clock->sleep(1);
+
+        // i'm lazy so we just blindly process the messages here until it does
+        // not emit more messages.
+
+        while ($transport->queue()->messages()) {
+            $clock->sleep(60);
+            $transport->process();
+        }
+
+        // we check the result
+
+        $this->assertEquals(1, $this->getOrderCountIn2030());
+    }
 }
