3f43cb44c723bedeb7b86375da0338c950978395
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-02-07 10:23:42 +0700

    fix: dependencies & other architectural issues

---
+++ tests/src/ArchitectureTests/Selectors.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/analytics package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Analytics\Tests\ArchitectureTests;
+
+use PHPat\Selector\Modifier\AllOfSelectorModifier;
+use PHPat\Selector\Selector;
+
+final readonly class Selectors
+{
+    private function __construct() {}
+
+    public static function selectAnalyticsCore(): AllOfSelectorModifier
+    {
+        return Selector::AllOf(
+            Selector::inNamespace('Rekalogika\Analytics'),
+            Selector::NOT(
+                Selector::inNamespace('Rekalogika\Analytics\Bundle'),
+            ),
+            Selector::NOT(
+                Selector::inNamespace('Rekalogika\Analytics\Tests'),
+            ),
+        );
+    }
+}
