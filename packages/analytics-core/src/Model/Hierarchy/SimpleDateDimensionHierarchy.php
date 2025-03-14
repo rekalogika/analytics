551e4940e85e1f1147f206b965df39ba6420b13a
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-03-15 02:03:39 +0700

    fix

--- packages/analytics-core/src/Model/Hierarchy/SimpleDateDimensionHierarchy.php
+++ packages/analytics-core/src/Model/Hierarchy/SimpleDateDimensionHierarchy.php
@@ -17,7 +17,6 @@ use Doctrine\ORM\Mapping\Embeddable;
 use Rekalogika\Analytics\Attribute\Hierarchy;
 use Rekalogika\Analytics\Model\Hierarchy\Trait\DayTrait;
 use Rekalogika\Analytics\Model\Hierarchy\Trait\MonthTrait;
-use Rekalogika\Analytics\Model\Hierarchy\Trait\QuarterTrait;
 use Rekalogika\Analytics\Model\Hierarchy\Trait\TimeZoneTrait;
 use Rekalogika\Analytics\Model\Hierarchy\Trait\YearTrait;
 use Rekalogika\Analytics\TimeZoneAwareDimensionHierarchy;
