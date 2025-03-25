116ed8b7cc40c58d9046b4c3b0f97411b251331e
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-03-25 15:22:54 +0700

    feat: unique dimensions collector

---
+++ tests/src/UnitTests/DimensionCollectorTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/analytics package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Analytics\Tests\UnitTests;
+
+use PHPUnit\Framework\TestCase;
+use Rekalogika\Analytics\Contracts\Dimension;
+use Rekalogika\Analytics\SummaryManager\SummarizerWorker\DimensionCollector\DimensionByKeyCollector;
+use Rekalogika\Analytics\SummaryManager\SummarizerWorker\Output\DefaultDimension;
+use Rekalogika\Analytics\Tests\App\Entity\Gender;
+use Rekalogika\Analytics\Util\TranslatableMessage;
+
+final class DimensionCollectorTest extends TestCase
+{
+    /**
+     * @var array<string,Dimension>
+     */
+    private array $dimensions = [];
+
+    private function addDimension(string $id, Dimension $dimension): void
+    {
+        $this->dimensions[$id] = $dimension;
+    }
+
+    private function getDimension(string $id): Dimension
+    {
+        if (!isset($this->dimensions[$id])) {
+            throw new \InvalidArgumentException(\sprintf('Dimension "%s" not found', $id));
+        }
+
+        return $this->dimensions[$id];
+    }
+
+    #[\Override]
+    public function setUp(): void
+    {
+        $this->addDimension('male', new DefaultDimension(
+            label: new TranslatableMessage('Male'),
+            key: 'gender',
+            rawMember: Gender::Male,
+            member: Gender::Male,
+            displayMember: Gender::Male,
+        ));
+
+        $this->addDimension('female', new DefaultDimension(
+            label: new TranslatableMessage('Female'),
+            key: 'gender',
+            rawMember: Gender::Female,
+            member: Gender::Female,
+            displayMember: Gender::Female,
+        ));
+
+        $this->addDimension('other', new DefaultDimension(
+            label: new TranslatableMessage('Other'),
+            key: 'gender',
+            rawMember: Gender::Other,
+            member: Gender::Other,
+            displayMember: Gender::Other,
+        ));
+
+        $this->addDimension('null', new DefaultDimension(
+            label: new TranslatableMessage('Unknown'),
+            key: 'gender',
+            rawMember: null,
+            member: null,
+            displayMember: 'Unknown',
+        ));
+    }
+
+    public function testDimensionCollector(): void
+    {
+        $collector = new DimensionByKeyCollector('gender');
+
+        $collector->addDimension([], $this->getDimension('female'));
+        $collector->addDimension([], $this->getDimension('male'));
+        $collector->addDimension([], $this->getDimension('other'));
+        $collector->addDimension([], $this->getDimension('null'));
+
+        $this->assertEquals([
+            Gender::Female,
+            Gender::Male,
+            Gender::Other,
+            null,
+        ], array_map(fn($dimension): mixed => $dimension->getRawMember(), iterator_to_array($collector->getResult())));
+    }
+}
