2d39a7de4276248131f2047452d5c742255e7d09
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-04-20 12:14:12 +0700

    feat: mandatory dimension

--- tests/src/App/Entity/OccupancyHistorySummary.php
+++ tests/src/App/Entity/OccupancyHistorySummary.php
@@ -13,17 +13,20 @@ declare(strict_types=1);
 
 namespace Rekalogika\Analytics\Tests\App\Entity;
 
-use Doctrine\Common\Collections\Order as DoctrineOrder;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Rekalogika\Analytics\AggregateFunction\Sum;
 use Rekalogika\Analytics\Attribute as Analytics;
 use Rekalogika\Analytics\Contracts\Model\Partition;
-use Rekalogika\Analytics\Model\Hierarchy\DateOnlyDimensionHierarchy;
+use Rekalogika\Analytics\DimensionValueResolver\TimeFormat;
 use Rekalogika\Analytics\Model\Partition\SingleLevelIntegerPartition;
 use Rekalogika\Analytics\Model\Summary;
+use Rekalogika\Analytics\Model\TimeInterval\Date;
+use Rekalogika\Analytics\Model\TimeInterval\DayOfMonth;
+use Rekalogika\Analytics\Model\TimeInterval\DayOfWeek;
+use Rekalogika\Analytics\Model\TimeInterval\DayOfYear;
 use Rekalogika\Analytics\ValueResolver\DateToIntegerResolver;
-use Rekalogika\Analytics\ValueResolver\PropertyValueResolver;
+use Rekalogika\Analytics\ValueResolver\TimeDimensionValueResolver;
 use Symfony\Component\Translation\TranslatableMessage;
 
 #[ORM\Entity()]
@@ -45,15 +48,54 @@ class OccupancyHistorySummary extends Summary
     // dimensions
     //
 
-    #[ORM\Embedded()]
+    #[ORM\Column(type: 'rekalogika_analytics_date', nullable: true)]
     #[Analytics\Dimension(
-        source: new PropertyValueResolver('date'),
-        label: new TranslatableMessage('Date'),
-        sourceTimeZone: new \DateTimeZone('Asia/Jakarta'),
-        summaryTimeZone: new \DateTimeZone('Asia/Jakarta'),
-        orderBy: DoctrineOrder::Ascending,
+        source: new TimeDimensionValueResolver(
+            property: 'date',
+            format: TimeFormat::Date,
+            sourceTimeZone: new \DateTimeZone('Asia/Jakarta'),
+            summaryTimeZone: new \DateTimeZone('Asia/Jakarta'),
+        ),
+        label: new TranslatableMessage('Date', domain: 'rekalogika_analytics'),
+        mandatory: true,
     )]
-    private DateOnlyDimensionHierarchy $date;
+    private ?Date $date;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    #[Analytics\Dimension(
+        source: new TimeDimensionValueResolver(
+            property: 'date',
+            format: TimeFormat::DayOfWeek,
+            sourceTimeZone: new \DateTimeZone('Asia/Jakarta'),
+            summaryTimeZone: new \DateTimeZone('Asia/Jakarta'),
+        ),
+        label: new TranslatableMessage('Day of Week', domain: 'rekalogika_analytics'),
+    )]
+    private ?DayOfWeek $dayOfWeek;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    #[Analytics\Dimension(
+        source: new TimeDimensionValueResolver(
+            property: 'date',
+            format: TimeFormat::DayOfMonth,
+            sourceTimeZone: new \DateTimeZone('Asia/Jakarta'),
+            summaryTimeZone: new \DateTimeZone('Asia/Jakarta'),
+        ),
+        label: new TranslatableMessage('Day of Month', domain: 'rekalogika_analytics'),
+    )]
+    private ?DayOfMonth $dayOfMonth;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    #[Analytics\Dimension(
+        source: new TimeDimensionValueResolver(
+            property: 'date',
+            format: TimeFormat::DayOfYear,
+            sourceTimeZone: new \DateTimeZone('Asia/Jakarta'),
+            summaryTimeZone: new \DateTimeZone('Asia/Jakarta'),
+        ),
+        label: new TranslatableMessage('Day of Year', domain: 'rekalogika_analytics'),
+    )]
+    private ?DayOfYear $dayOfYear;
 
     #[ORM\Column(enumType: Gender::class, nullable: true)]
     #[Analytics\Dimension(
@@ -87,9 +129,24 @@ class OccupancyHistorySummary extends Summary
         return $this->gender;
     }
 
-    public function getDate(): DateOnlyDimensionHierarchy
+    public function getDate(): ?Date
     {
         return $this->date;
+    }
+
+    public function getDayOfWeek(): ?DayOfWeek
+    {
+        return $this->dayOfWeek;
+    }
+
+    public function getDayOfMonth(): ?DayOfMonth
+    {
+        return $this->dayOfMonth;
+    }
+
+    public function getDayOfYear(): ?DayOfYear
+    {
+        return $this->dayOfYear;
     }
 
     public function getCount(): ?int
