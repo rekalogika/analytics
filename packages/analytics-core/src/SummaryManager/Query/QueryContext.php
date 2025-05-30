b5f6158d23d6f7dbf250253eb6363477c57e0f92
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-05-30 19:34:20 +0700

    refactor: consolidate context

--- packages/analytics-core/src/SummaryManager/Query/QueryContext.php
+++ packages/analytics-core/src/SummaryManager/Query/QueryContext.php
@@ -13,35 +13,21 @@ declare(strict_types=1);
 
 namespace Rekalogika\Analytics\SummaryManager\Query;
 
-use Doctrine\DBAL\ArrayParameterType;
-use Doctrine\DBAL\ParameterType;
 use Rekalogika\Analytics\SimpleQueryBuilder\SimpleQueryBuilder;
 
+/**
+ * @deprecated
+ */
 final class QueryContext
 {
     public function __construct(
         private readonly SimpleQueryBuilder $queryBuilder,
     ) {}
 
-    /**
-     * Path is a dot-separated string that represents a path to a property of an
-     * entity. This method resolves the path to a DQL path, and joins the
-     * necessary tables. If the path resolves to a related entity, you can
-     * prefix the path with * to force a join, and return the alias.
-     */
     public function resolvePath(string $path): string
     {
         return $this->queryBuilder->resolve($path);
     }
 
-    /**
-     * Doctrine 2 does not have createNamedParameter method in QueryBuilder,
-     * so we do it manually here.
-     */
-    public function createNamedParameter(
-        mixed $value,
-        int|string|ParameterType|ArrayParameterType|null $type = null,
-    ): string {
-        return $this->queryBuilder->createNamedParameter($value, $type);
-    }
+
 }
